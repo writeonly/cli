499ca7df1e7914d3b483a748819c3712d794efc6
Metrics & Logging <9075871+writeonly@users.noreply.github.com>
2018-10-10 17:40:27 +0200

    add git flow

--- git_config.sh
+++ git_config.sh
@@ -7,3 +7,19 @@ git config --global alias.pru '!git pur && git update'
 git config --global alias.master '!git checkout master && git update'
 git config --global alias.develop '!git checkout develop && git update'
 git config --global alias.tig '!tig'
+
+# git flow
+git config --global alias.fi 'flow init'
+# git flow feature
+git config --global alias.ffstart 'flow feature start'
+git config --global alias.fffinish 'flow feature finish '
+git config --global alias.ffpublish 'flow feature publish'
+git config --global alias.ffpull 'flow feature pull origin'
+git config --global alias.fftrack 'git flow feature track'
+# git flow release
+git config --global alias.frstart 'flow release start'
+git config --global alias.frpusblish 'flow release publish'
+git config --global alias.frfinish 'flow release finish'
+# git flow hotfix
+git config --global alias.fhstart 'flow hotfix start'
+git config --global alias.fhfinish 'flow hotfix finish'
