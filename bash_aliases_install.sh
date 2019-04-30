a207701fc3fcab545d4ef4b37a0fbef76454a56d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2019-04-30 12:29:10 +0200

    Update bash_aliases_install.sh

--- bash_aliases_install.sh
+++ bash_aliases_install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 cd ~
+rm -rf writeonly-cli
 git clone https://github.com/writeonly/cli.git writeonly-cli
 cat <<EOT >> ~/.bashrc
 
 if [ -f ~/writeonly-cli/bash_aliases ]; then
     . ~/writeonly-cli/bash_aliases
 fi
-export PATH="$PATH:~/writeonly-cli"
+export PATH="\$PATH:~/writeonly-cli"
 EOT
